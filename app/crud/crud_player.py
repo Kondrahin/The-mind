483e6597ed5347e993c0a0ac7d8cdaca115b0ef8
Kondrahin <tony.kondrahin@yandex.ru>
2021-03-30 23:33:38 +0300

    Player must be tide to the room

--- app/crud/crud_player.py
+++ app/crud/crud_player.py
@@ -1,22 +1,25 @@
+import logging
 from sqlalchemy.orm import Session
-
 from app.models import Player
+
+logging.basicConfig(level=logging.INFO)
+log = logging.getLogger(__name__)
 
 
 class CRUDPlayer:
 
-    def create(self, db: Session, *, player_token: str, player_nick: str) -> Player:
+    def create(self, db: Session, *, player_token: str, player_nick: str, room_token: str) -> Player:
         db_obj = Player(
             id=player_token,
             nick=player_nick,
+            room_token=room_token
         )
         db.add(db_obj)
         db.commit()
         db.refresh(db_obj)
         return db_obj
 
-
-    def get(self, db:Session, *, player_token):
+    def get(self, db: Session, *, player_token):
         pass
 
     def connect(self, db: Session, *, player: Player, room_token):
